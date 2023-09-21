04d2444ae2036b0ab560c58a45c7487874523fd6
Daniello109 <legolas_lorien@hotmail.com>
2023-09-21 10:58:36 +0200

    quete finie

---
+++ Photo.java
@@ -0,0 +1,17 @@
+public class Photo {
+
+ public Photo(String url, String filename, int width, int height){
+
+    url=this.url;
+    filename=this.filename;
+    width=this.width;
+    height=this.height;
+ }
+
+
+
+ private String url;
+ private String filename;
+ private int width;
+ private int height;
+}
---
+++ User.java
@@ -0,0 +1,10 @@
+
+import java.util.Date;
+import java.util.List;
+
+public class User {
+    Date birthdate;
+    int size;
+    List <Photo> photos;
+    Address address; 
+}
